ddce00eaa6a6a8fcedded610df8602bbdf97fb57
chdonncha <chdonncha@gmail.com>
2015-12-01 17:25:39 +0000

    Added example project

--- create_tables.sql
+++ create_tables.sql
@@ -1,32 +1,34 @@
 
 create table users
             (
-            username varchar2(20) primary key not null , 
-            name varchar2(60) not null , 
-            email varchar2(60) not null , 
+            username varchar(20) primary key not null , 
+            name varchar(60) not null , 
+            email varchar(60) not null , 
             password varchar(20) not null ,
-            confpassword varchar2(20) not null
+            confpassword varchar(20) not null
             );
 
+INSERT INTO users VALUES ("johnerz84", "John", "JohnDoe@doemail.com", "123", "123");            
+            
 create table debate
-            (question varchar2(60) primary key not null,
-            topic varchar2(40) not null,
-            yes number (6) null, 
-            no number (6) null,
-            username varchar2(20) not null ,
+            (question varchar(60) primary key not null,
+            topic varchar(40) not null,
+            yes int  null, 
+            no int  null,
+            username varchar(20) not null ,
             FOREIGN KEY (username) REFERENCES users(username));
 
 create table comments
-            (comment_id number(6) primary key not null , 
-            commentdata varchar2(60) not null , 
-            username varchar2(20) not null , 
-            question varchar2(60) not null , 
+            (comment_id int primary key not null , 
+            commentdata varchar(60) not null , 
+            username varchar(20) not null , 
+            question varchar(60) not null , 
             FOREIGN KEY (username) REFERENCES users(username) ,
             FOREIGN KEY (question) REFERENCES debate(question));
 
 create table hasvoted
-            (vote_id number(6) primary key not null , 
-            username varchar2(20) not null , 
-            question varchar2(60) not null ,
+            (vote_id int primary key not null , 
+            username varchar(20) not null , 
+            question varchar(60) not null ,
             FOREIGN KEY (username) REFERENCES users(username) ,
             FOREIGN KEY (question) REFERENCES debate(question));
